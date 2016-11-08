a1d1d8c0d0f43d34ce9b290659104a29aa41a636
ModuloM <modulom@modulom.net>
2016-11-08 13:53:09 +0100

    [fix] fix email pattern validation regexp, add + and fix -

--- src/scripts/services/validator.service.js
+++ src/scripts/services/validator.service.js
@@ -35,7 +35,7 @@ validatorService.isEmpty = (value) => value === undefined || value === null || v
  * @returns {boolean}
  */
 validatorService.isEmailValid = (value) => !validatorService.isEmpty(value) &&
-  /^(?=[\S]{6,50}$)[A-Za-z0-9._-]+@(?:[A-Za-z0-9-]{1,}\.){1,}[A-Za-z]{2,}$/.test(value)
+  /^(?=[\S]{6,50}$)[A-Za-z0-9._\-\+]+@(?:[A-Za-z0-9-]{1,}\.){1,}[A-Za-z]{2,}$/.test(value)
 
 /**
  * Revalidate email pattern validator
