9c6406c761ad508da73ea59af6756a9c8af8a485
ModuloM <modulom@modulom.net>
2017-04-12 18:09:06 +0200

    [styleguide] ProjectConfigComponent - fix story by passing a store

--- src/scripts/components/projectConfig/ProjectConfig.component.story.js
+++ src/scripts/components/projectConfig/ProjectConfig.component.story.js
@@ -1,21 +1,47 @@
 import React from 'react'
-import { storiesOf, linkTo } from '@kadira/storybook'
+import { Provider } from 'react-redux'
+import { storiesOf } from '@kadira/storybook'
 import { IntlProvider } from 'react-intl'
 
 // contexte
+import configureStore from '../../store/configureStore'
 import en from '../../i18n/en'
 
 // component to story
 import ProjectConfig from './ProjectConfig.component'
 
+const initialState = {
+  projectConfig: {
+    list: {
+      'ProjectConfigId': {
+        id: 'ProjectConfigId',
+        stacks: [
+          {
+            bricks: [
+              {
+                state: 'RUNNING'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+
+const storeInitial = configureStore(initialState)
+
+
 storiesOf('ProjectConfig', module)
   .add('defaut', () => (
-    <IntlProvider locale="en" messages={ en }>
-      <ProjectConfig
-        id="ProjectConfigId"
-        name="ProjectName"
-        status={ 'RUNNING' }
-        usersNumber={ 12 }
-      />
-    </IntlProvider>
+    <Provider store={storeInitial}>
+      <IntlProvider locale="en" messages={ en }>
+        <ProjectConfig
+          id="ProjectConfigId"
+          name="ProjectName"
+          status={ 'RUNNING' }
+          usersNumber={ 12 }
+        />
+      </IntlProvider>
+    </Provider>
   ))
